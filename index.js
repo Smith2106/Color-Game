ae40d374e59c0f09409347771b14535f91f10e8f
Elijah Smith <elijahs2106@gmail.com>
2018-07-07 19:40:18 -0400

    Add basic functionality and styles via hardcoding in js

---
+++ index.js
@@ -0,0 +1,27 @@
+const colors = [
+    'rgb(255, 0, 0)',
+    'rgb(255, 255, 0)',
+    'rgb(0, 255, 0)',
+    'rgb(0, 255, 255)',
+    'rgb(0, 0, 255)',
+    'rgb(255, 0, 255)',
+];
+
+const squares = document.querySelectorAll('.square');
+let pickedColor = colors[3];
+const colorDisplay = document.querySelector('#colorDisplay');
+
+colorDisplay.textContent = pickedColor;
+
+squares.forEach((square, i) => {
+    square.style.backgroundColor = colors[i];
+    square.addEventListener('click', (e) => {
+        let clickedColor = e.target.style.backgroundColor;
+        if (clickedColor === pickedColor) {
+            alert('Correct!');
+        }
+        else {
+            alert('Wrong!');
+        }
+    });
+});
